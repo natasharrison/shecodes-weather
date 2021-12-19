338e0c4b7f04511d9d00c7ec7d6e926e0ea1698e
natasharrison <natasharrison@gmail.com>
2021-12-19 09:43:48 -0700

    javascript bug fixed - able to enter city prompt

--- src/index.js
+++ src/index.js
@@ -1,34 +1,37 @@
-// Include the JavaScript file provided in your project, when loading your project, ask the user "Enter a city" (example: Paris), alert "It is currently 19°C (66°F) in Paris with a humidity of 80%"
-
-// If the city doesn't exist in the object (i.e: Sydney), alert "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney". Since this is an alert, the link shouldn't be clickable.
-
-// Add this behavior to your project and submit the CodeSandbox URL
-
-// Note: Please round the values in the Alert to the nearest whole number (no decimal points, e.g. 5.45 should be rounded to 5).
-
-let weather = [
-    {
-        name: "New York",
-        temp: 15,
-        humidity: 10,
-    },
-    {
-        name: "London",
-        temp: 9,
-        humidity: 10,
-    },
-    {
-        name: "Cape Town",
-        temp: 25,
-        humidity: 20,
-    },
-];
+let weather = {
+  "paris": {
+    temp: 19.7,
+    humidity: 80
+  },
+  "tokyo": {
+    temp: 17.3,
+    humidity: 50
+  },
+  "lisbon": {
+    temp: 30.2,
+    humidity: 20
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100
+  },
+  "moscow": {
+    temp: -5,
+    humidity: 20
+  }
+};
 
 let city = prompt("Please enter a city");
+city = city.toLowerCase();
 
-if ((city === "New York", "London", "Cape Town")) {
+if (weather[city] !== undefined) {
+    let temperature = weather[city].temp;
+    let humidity = weather[city].humidity;
+    let celsiusTemperature = Math.round(temperature);
+    let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
+
     alert(
-        `It is currently ${Math.round(temp)} in ${city} with a humidity of ${Math.round(humidity)}`
+        `It is currently ${fahrenheitTemperature}°F (${celsiusTemperature}°C) in ${city} with a humidity of ${Math.round(humidity)}%.`
     );
 } else {
     alert(
